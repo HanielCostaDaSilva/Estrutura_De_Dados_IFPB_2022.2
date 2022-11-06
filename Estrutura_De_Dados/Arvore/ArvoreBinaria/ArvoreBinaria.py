c904d7f479c6448e9ccbc3fefa9f36f02744165e
Haniel Costa da Silva <hanielcosta.ti@gmail.com>
2022-11-06 14:30:47 -0300

    Adicionado o assunto de Árvore binária e implementado uma main interativa para tal

--- Estrutura_De_Dados/Arvore/ArvoreBinaria/ArvoreBinaria.py
+++ Estrutura_De_Dados/Arvore/ArvoreBinaria/ArvoreBinaria.py
@@ -1,184 +1,277 @@
-#== == == == Onde os elementos da árvore são criados
-class No:
-    def __init__(self, carga:any):
-        self.carga = carga
-        self.esq = None
-        self.dir = None
- 
-    def __str__(self):
-        return f'{self.carga}'
- 
- 
-class Arvore:
-    def __init__(self,valorInicial= None) -> None:
-        self.__raiz= No(valorInicial) if valorInicial!= None else valorInicial
-        self.__cursor= self.__raiz
-    
-    def preordem(self):
-        if self.__cursor is None: # Se Chegar a um nó folha
-            return # não faz nada
-        self.preordem(self.__cursor.esq) #== == == Ele execultará até a última esquerda do nó
-        self.preordem(self.__cursor.dir)#== == == Quando terminar de percorrer a esquerda de um nó, ele então irá percorrer a direita deste
- 
-    def emordem(self):
-        if self.__cursor is None: #Se chegar a um nó folha
-            return # encerra a recursividade
-        self.emordem(self.__cursor.esq) #ela execultará a recursividade até que chegue no nó mais a esquerda 
-        self.emordem(self.__cursor.dir) #agora percorrerá a sua direita
- 
-#== == == == Esta busca inicirá sempre no nó folha mais extremo, inicia sempre pela esquerda. 
-    def posordem(self): 
-        if self.__cursor is None: #chegou no nó mais extremo
-            return #termina a recursividade
-        self.posordem(self.__cursor.esq) #primeiro, busca se a algum nó a esquerda do estudado
-        self.posordem(self.__cursor.dir) #depois, vê se não a nenhum outro a direita
-         
- 
-#== == == == Essa é a busca mais simples, o usuário passa uma chave e então o programa percorrerá toda árvore em busca da chave.    
-    def busca(self, chave)->bool:
-        if self.__cursor is None:
-            return False
-        if self.__cursor.carga == chave:
-            return True
-        if ( self.busca(chave, self.__cursor.esq)):
-            return True
-        else:
-            return self.busca(chave, self.__cursor.dir)
- 
-    def estaVazia(self):
-        if self.__raiz== None:
-            return True
-        else: 
-            return False
-    
-    def getRaiz(self):
-        if self.__raiz != None:
-            return self.__raiz.carga;
-        else: return None
-    
-    def get__cursor(self):
-        return self.__cursor
-    
-    def descer_A_Esquerda(self):
-        if not(self.estaVazia()) and self.__cursor.esq != None:
-                self.__cursor= self.__cursor.esq
-    
-    def descer_A_Direita(self):
-        if not(self.estaVazia()):
-            if self.__cursor.dir != None:
-                self.__cursor= self.__cursor.dir
-    
-    def resetCursor(self):
-        if not(self.estaVazia):
-            self.__cursor=self.__raiz
- 
-    def count(self,cursor):
-        contador=0
-        if self.__cursor is None:
-            return 0
- 
-        contador+=self.count(self.__cursor.esq) #primeiro, busca se a algum nó a esquerda do estudado
-        contador+=self.count(self.__cursor.dir) #depois, vê se não a nenhum outro a direita        
-        return contador + 1
- 
-    def __len__(self):
-        return self.count()
-    
-    def criarRaiz(self, valor:any):
-        if self.__raiz is None:
-            self.__raiz== No(valor)
- 
-#== == == == Onde os elementos da árvore são criados
-class No:
-    def __init__(self, carga:any):
-        self.carga = carga
-        self.esq = None
-        self.dir = None
- 
-    def __str__(self):
-        return f'{self.carga}'
- 
- 
-class Arvore:
-    def __init__(self,valorInicial= None) -> None:
-        self.__raiz= No(valorInicial) if valorInicial!= None else valorInicial
-        self.__cursor= self.__raiz
-    
-    def preordem(self):
-        if self.__cursor is None: # Se Chegar a um nó folha
-            return # não faz nada
-        self.preordem(self.__cursor.esq) #== == == Ele execultará até a última esquerda do nó
-        self.preordem(self.__cursor.dir)#== == == Quando terminar de percorrer a esquerda de um nó, ele então irá percorrer a direita deste
- 
-    def emordem(self):
-        if self.__cursor is None: #Se chegar a um nó folha
-            return # encerra a recursividade
-        self.emordem(self.__cursor.esq) #ela execultará a recursividade até que chegue no nó mais a esquerda 
-        self.emordem(self.__cursor.dir) #agora percorrerá a sua direita
- 
-#== == == == Esta busca inicirá sempre no nó folha mais extremo, inicia sempre pela esquerda. 
-    def posordem(self): 
-        if self.__cursor is None: #chegou no nó mais extremo
-            return #termina a recursividade
-        self.posordem(self.__cursor.esq) #primeiro, busca se a algum nó a esquerda do estudado
-        self.posordem(self.__cursor.dir) #depois, vê se não a nenhum outro a direita
-         
- 
-#== == == == Essa é a busca mais simples, o usuário passa uma chave e então o programa percorrerá toda árvore em busca da chave.    
-    def busca(self, chave)->bool:
-        if self.__cursor is None:
-            return False
-        if self.__cursor.carga == chave:
-            return True
-        if ( self.busca(chave, self.__cursor.esq)):
-            return True
-        else:
-            return self.busca(chave, self.__cursor.dir)
- 
-    def estaVazia(self):
-        if self.__raiz== None:
-            return True
-        else: 
-            return False
-    
-    def getRaiz(self):
-        if self.__raiz != None:
-            return self.__raiz.carga;
-        else: return None
-    
-    def get__cursor(self):
-        return self.__cursor
-    
-    def descer_A_Esquerda(self):
-        if not(self.estaVazia()) and self.__cursor.esq != None:
-                self.__cursor= self.__cursor.esq
-    
-    def descer_A_Direita(self):
-        if not(self.estaVazia()):
-            if self.__cursor.dir != None:
-                self.__cursor= self.__cursor.dir
-    
-    def resetCursor(self):
-        if not(self.estaVazia):
-            self.__cursor=self.__raiz
- 
-    def count(self,cursor):
-        contador=0
-        if self.__cursor is None:
-            return 0
- 
-        contador+=self.count(self.__cursor.esq) #primeiro, busca se a algum nó a esquerda do estudado
-        contador+=self.count(self.__cursor.dir) #depois, vê se não a nenhum outro a direita        
-        return contador + 1
- 
-    def __len__(self):
-        return self.count()
-    
-    def criarRaiz(self, valor:any):
-        if self.__raiz is None:
-            self.__raiz== No(valor)
- 
- 
- 
- 
+from enum import Enum
 
+#== == == ==Criador da Exceções da Árvore binário
+class BinaryArborException(Exception):
+    def __init__(self, msg) -> None:
+        super().__init__('Binary Arbor Exception: ', msg)
+
+#== == == ==Criador da Exceções dos Nós
+class NodeException(Exception):
+    def __init__(self, msg) -> None:
+        super().__init__('Node Exception: ', msg)        
+#== == == ==Aqui onde os nós são criados
+class Node:
+    def __init__(self,carga:any):
+        self.carga = carga
+        self.esq = None
+        self.dir = None
+
+    def __str__(self):
+        return str(self.carga)
+
+#== == == ==Enumera os elementos contido  na Arvore Binária
+class Origem(Enum):
+    RAIZ = 1
+    CURSOR = 2
+    
+#== == == ==Classe que contém todos os métodos aárvore binária
+class ArvoreBinaria:
+    #== == == Método que cria a raiz               
+    def __init__(self, carga_da_raiz=None):
+        self.__raiz=Node(carga_da_raiz) if carga_da_raiz != None else carga_da_raiz#== == Caso não seja declarado um nó raiz, a árvore existirá, porém vazia.
+        self.__cursor= self.__raiz
+
+    ''' Exemplo de árvore binária
+                    A
+                B         C
+             D         E      F
+          I    J         K
+    '''
+    @property
+    def raiz(self):
+        return self.__raiz
+    
+    @property
+    def cursor(self):
+        return self.__cursor
+    
+    #== == == Método que confere se a raiz está vazia            
+    def estaVazia(self)->bool:
+        if self.__raiz==None: return True
+        else: return False
+    
+    #== == == Retorna o Nó raiz, caso ele exista            
+    def getRaiz(self)->any:
+        try:
+            assert self.__raiz != None 
+            return self.__raiz
+        except AssertionError:
+            raise BinaryArborException('NO ROOT!')
+
+     #== == == Retorna o nó que está sendo apontado pelo cursor            
+    def getCursor(self)->any:
+        try:
+            assert self.__raiz != None
+        
+            return self.__cursor
+        except AssertionError:
+            raise BinaryArborException('NO ROOT!')
+    
+    #== == == Retorna os elementos em préordem
+    '''Pré-Ordem: Mostra o nó e depois se dirige para a sua esquerda, depois prossegue para a direita.
+    Este seria a amostragem da pré-odem da árvore do começo: IDJBAEKCF'''            
+    def preordem(self, origem:Origem=Origem.RAIZ):
+        try:
+            assert not(self.estaVazia())
+            if origem==Origem.RAIZ.value: self.__preordem(self.__raiz)
+            elif origem==Origem.CURSOR.value: self.__preordem(self.__cursor)
+        except AssertionError:
+            raise BinaryArborException('NO ROOT!')
+    
+    def __preordem(self, no:Node):
+        if no==None:
+            return
+        print(no.carga)
+        self.__preordem(no.esq)
+        self.__preordem(no.dir)
+        
+    #== == == Retorna os elementos em in-ordem
+    '''In-Ordem: Se Dirige para extrema esquerda, Mostra o Nó, depois mostra o Nó acima e prossegue para a sua direita.
+    Este seria a amostragem da in-odem da árvore do começo: ABDIJCEK'''            
+    def emordem(self, origem:Origem=Origem.RAIZ.value):
+        try:
+            assert not(self.estaVazia())
+            if origem==Origem.RAIZ.value: self.__emordem(self.__raiz)
+            elif origem==Origem.CURSOR.value: self.__emordem(self.__cursor)
+        except AssertionError:
+            raise BinaryArborException('NO ROOT!')
+
+
+    def __emordem(self, no:Node):
+        if no==None:
+            return
+        self.__emordem(no.esq)
+        print(no)
+        self.__emordem(no.dir)
+        
+    #== == == Retorna os elementos em pós-ordem
+    '''Pós-Ordem: Dirige-se para a direita e depois para esquerda. Em seguida, mosta o nó
+    Este seria a amostragem da pré-odem da árvore do começo: FKECJIDBA'''      
+    def posordem(self, origem:Origem=Origem.RAIZ.value):
+        try:
+            assert not(self.estaVazia())
+            if origem==Origem.RAIZ.value: self.__posordem(self.__raiz)
+            elif origem==Origem.CURSOR.value: self.__posordem(self.__cursor)
+        except AssertionError:
+            raise BinaryArborException('NO ROOT!')
+
+    def __posordem(self, no:Node):
+        if no==None:
+            return
+        self.__posordem(no.dir)
+        self.__posordem(no.esq)
+        print(no)
+        #== == == Move o cursor para a equerda do nó      
+    def descerEsquerda(self):
+        try:
+            assert not(self.estaVazia())
+            if self.__cursor.esq==None:
+                raise(f'O cursor está no Nó: {self.__cursor.carga} ele não possui Nó à esquerda!')
+            self.__cursor=self.__cursor.esq
+        except AssertionError:
+            raise BinaryArborException('NO ROOT!')
+        
+    #== == == Move o cursor para a direita do nó 
+    
+    def descerDireita(self):
+        try:
+            assert not(self.estaVazia())
+            if self.__cursor.dir==None:
+                raise(f'O cursor está no Nó: {self.__cursor.carga} ele não possui Nó à direita!')
+            self.__cursor=self.__cursor.dir
+        except AssertionError:
+            raise BinaryArborException('NO ROOT!')
+
+#== == == Move o cursor de volta para a raiz. 
+    def resetCursor(self):
+        try:
+            assert not(self.estaVazia())
+            self.__cursor= self.__raiz
+        except AssertionError:
+            raise BinaryArborException('NO ROOT!')
+
+#== == == Insere um nó a equerda do cursor. 
+    def addFilhoEsquerdo(self, carga)->bool:
+        try:
+            assert not(self.estaVazia())
+            if self.__cursor.esq!=None:
+                raise NodeException('THERE IS A NODE AT LEFT')
+            
+            self.__cursor.esq=Node(carga)
+        except AssertionError:
+            raise BinaryArborException('NO ROOT!')
+
+#== == == Insere um nó a direita do cursor.     
+    def addFilhoDireito(self, carga)->bool:
+        
+        try:
+            assert not(self.estaVazia())
+            if self.__cursor.dir!=None:
+                raise NodeException('THERE IS A NODE AT RIGHT')
+            
+            self.__cursor.dir=Node(carga)
+        except AssertionError:
+            raise BinaryArborException('NO ROOT!')
+
+    #== == == Retorna a quantiade de Nós
+    
+    def __len__(self):
+        return self.__count(self.__raiz)
+    
+    
+    def __count(self, no:Node)->int:
+
+        if no==None:
+            return 0 #== == Chegou ao fim.
+        
+        quantidadeNo= 1 +self.__count(no.esq) # quantidade nó é criado e é somado com a quantiade de nós a sua  do nó a esquerda
+        quantidadeNo+= self.__count(no.dir) # quando não há nós a esquerda, ele tentará a sua direita.
+        return quantidadeNo #finalizado, ele retornará a soma.
+
+    #se orientado ao cursor
+    def busca(self, chave,origem):
+        try:
+            assert not(self.estaVazia())
+            if origem== Origem.RAIZ.value: self.__busca(chave,self.__raiz)
+            elif origem== Origem.CURSOR.value: self.__busca(chave,self.__cursor)
+        except AssertionError:
+            raise BinaryArborException('NO ROOT!')
+    
+    #== == == == procura se existe um determinado. se orientando pelo cursor
+    def __busca(self, chave, node:Node) ->bool:
+        
+        if node==None: # não achou o nó
+            return False
+        
+        elif node.carga==chave:
+            return True
+        
+        elif self.__busca(chave,node.esq): #busca pela esquerda
+            return True
+        
+        elif self.__busca(chave,node.dir): #busca pela direita
+            return True
+    
+    #== == == Esse método é orientado ao cursor. Ele excluir o nó a direita ou a esquerda do cursor
+    def removeNoFolha(self,chave):
+        try:
+            assert not(self.estaVazia())
+            #== == Testa se o nó á ser removido esquerda
+            if self.__cursor.esq!= None:
+                
+                if self.__cursor.esq.carga==chave and \
+                self.__cursor.esq.esq ==None and self.__cursor.esq.dir==None:
+                
+                    self.__cursor.esq= None  #== O nó que o cursor está apontará para o None em sua esquerda.
+                
+                else:
+                    raise BinaryArborException('THIS NOT A LEAF NODE')         
+            
+            #== == Testa se o nó da direita é nó folha
+            elif self.__cursor.dir!=None:
+                if self.__cursor.dir.carga==chave and \
+                    self.__cursor.dir.esq== None and self.__cursor.dir.dir==None:
+                        self.__cursor.dir= None
+                
+                else:
+                    raise BinaryArborException('THIS NOT A LEAF NODE')
+            
+            else:
+                raise BinaryArborException('KEY NOT FOUND')
+        
+        except AssertionError:
+            raise BinaryArborException('NO ROOT!')   
+        
+        
+    def go(self, chave:int )->Node:
+        return self.__go(chave,self.__raiz)
+    
+    def __go(self, chave:int, Node:Node)->Node:
+        if Node is None:
+            return None
+        if Node.carga == chave:
+            return Node
+        resultado = self.__go(chave, Node.esq)
+        if ( resultado ):
+            return resultado
+        else:
+            return self.__go(chave, Node.dir)
+
+    
+    #== == == Método que remove todos os nós de maneira que não deixe vestígios na memória 
+    def esvazia(self):
+        try:
+            assert not(self.estaVazia())
+            self.__raiz=self.__libera(self.__raiz)
+            
+        except AssertionError:
+            raise BinaryArborException('NO ROOT!')
+        
+    def __libera(self,proximoNo)->None:
+        
+        if ( not self.estaVazia() and proximoNo != None):
+            
+            proximoNo.esq= self.__libera(proximoNo.esq)
+            
+            proximoNo.dir= proximoNo=self.__libera(proximoNo.dir)
+        return None
--- Estrutura_De_Dados/Arvore/ArvoreBinaria/main.py
+++ Estrutura_De_Dados/Arvore/ArvoreBinaria/main.py
@@ -1,62 +1,153 @@
-from ArvoreBinaria import *
+from ArvoreBinaria import ArvoreBinaria,NodeException, BinaryArborException
+import time
+import os
 
-#== == == == Este método percorrera toda a árvore execultando primeiramente dá raiz e em seguida da esquerda de todos os nós
-def preordem(no_inicial):
-    if no_inicial is None: # Se Chegar a um nó folha
-        return # não faz nada
-    print(f'{no_inicial.carga}', end=' ')
-    preordem(no_inicial.esq) #== == == Ele execultará até a última esquerda do nó
-    preordem(no_inicial.dir)#== == == Quando terminar de percorrer a esquerda de um nó, ele então irá percorrer a direita deste
+arv = ArvoreBinaria('A')
 
-#== == == == Esta busca inicirá sempre da folha a mais esquerda. 
-def emordem(no_inicial):
-    if no_inicial is None: #Se chegar a um nó folha
-        return # encerra a recursividade
-    emordem(no_inicial.esq) #ela execultará a recursividade até que chegue no nó mais a esquerda 
-    print(f'{no_inicial.carga}', end=' ') #caso já tenha percorrido o nó mais esquerda deste execultára algum comoando
-    emordem(no_inicial.dir) #agora percorrerá a sua direita
+def MostrarDicionario(dicionario:dict, Chave:str = '1'):
 
+    if dicionario.get(Chave,False)==False:
+        return
+    print(f'{Chave :^5}=> {dicionario[Chave]:^5} ')
+    MostrarDicionario(dicionario, str(int(Chave) +1 ) )
+        
 
-#== == == == Esta busca inicirá sempre no nó folha mais extremo, inicia sempre pela esquerda. 
-def posordem(no_inicial): 
-    if no_inicial is None: #chegou no nó mais extremo
-        return #termina a recursividade
-    posordem(no_inicial.esq) #primeiro, busca se a algum nó a esquerda do estudado
-    posordem(no_inicial.dir) #depois, vê se não a nenhum outro a direita
-    
-    print(f'{no_inicial.carga}', end=' ') # caso as duas condições acima sejam atendidas, ou já tenha sido execultada as suas extremidades, o nó será executado. 
+opcoesArvore={
+    '1':'Mostrar Nós em Pré-Ordem',
+    '2':'Mostrar Nós em In-Ordem',
+    '3':'Mostrar Nós em Pós-Ordem',
+    '4':'Mostrar o Valor da Raiz',
+    '5':'Mostrar o Valor do Cursor',
+    '6':'Mover o Cursor para esquerda',
+    '7':'Mover o Cursor para direita',
+    '8':'Adicionar Nó a esquerda',
+    '9':'Adicionar Nó a direita',
+    '10':'Buscar se existe um Nó.',
+    '11':'Remover Nó folha.',
+    '12':'Mover o cursor atrás de um nó específico',
+    '13':'Esvaziar a Árvore',
+    '14':'Mostrar o tamanho da árvore',
+    '15':'finalizar o programa',
+}
 
-#== == == == Essa é a busca mais simples, o usuário passa uma chave e então o programa percorrerá toda árvore em busca da chave. 
-def busca(chave, no_inicial)->bool:
-    if no_inicial is None:
-        return False
-    if no_inicial.carga == chave:
-        return True
-    if ( busca(chave, no_inicial.esq)):
-        return True
-    else:
-        return busca(chave, no_inicial.dir)
+opcoesArvoreChaves= list(opcoesArvore.keys())
+while True:
+    try:
+        print()
+        print('Escolha uma das seguintes opções: ')
+        MostrarDicionario(opcoesArvore)
+        escolha=input('\n escolha: ')
+#== == == == == ==        
+        if escolha== '1' or escolha== '2' or escolha== '3' or escolha=='10'  or escolha=='11':
+            
+            origem= str.lower(input('Digite a Origem: (raiz/cursor) '))
+            if origem=='raiz':
+                origem=1
+            elif origem=='cursor':
+                origem=2
+            else:
+                raise(Exception('por favor, escolha: raiz ou cursor '))
+            
+            if escolha=='1': #== == == Mostra a árvore em pré-ordem
+                arv.preordem(origem)
+            
+            elif escolha=='2':#== == == Mostra a árvore em in-ordem
+                arv.emordem(origem)
+            
+            elif escolha=='3':#== == == Mostra a árvore em pós-ordem
+                arv.posordem(origem)
+                
+            elif escolha=='10' or escolha=='11': 
+                NodeValue= input('digite o valor do Nó: ')
+                
+                if escolha=='10':
+                    arv.busca(NodeValue,origem)
+                if escolha=='11': #== == == Procura a existência de um nó à partir de uma determinada origem
+                    arv.busca(NodeValue,origem)
+            
+        
+        elif escolha=='4': #== == == Mostra o valor da raiz
+            print(arv.getRaiz())
+            
+        elif escolha=='5': #== == == Mostra o valor do cursor
+            print(arv.getCursor())
+        
+        elif escolha=='6': #== == == Move o cursor à sua esquerda
+            print(arv.descerEsquerda())
+        
+        elif escolha=='7': #== == == Move o cursor à sua direita
+            print(arv.descerDireita())
 
-raiz = No(10)
-raiz.esq = No(32)
-raiz.dir = No(23)
-cursor = raiz.esq # cursor desce para o nó 32
-cursor.esq = No(12)
-cursor.dir = No(40)
-cursor = cursor.esq
-cursor.esq = No(5)
-cursor = raiz.dir # cursor desce para o nó 23, a partir do raiz
-cursor.dir = No(30)
-cursor = cursor.dir # cursor desce para o nó 30
-cursor.esq = No(60)
-cursor = cursor.esq # cursor desce par ao nó 60
-cursor.dir = No(22)
+#== == == == == ==
+        elif escolha=='8' or escolha=='9' or escolha=='12':
+            NodeValue= input('digite o valor do Nó: ')
+            
+            if escolha=='8': #== == == Adiciona um nó à esquerda do cursor
+                arv.addFilhoEsquerdo(NodeValue)
+                print(f'Nó {NodeValue} adicionado a esquerda!')
+            
+            elif escolha=='9': #== == == Adiciona um nó à direita do cursor
+                arv.addFilhoDireito(NodeValue)
+                print(f'Nó {NodeValue} adicionado a direita!')
+            
+            elif escolha=='12': #== == == Move o cursor para um determinado nó
+                arv.go(NodeValue)
+                print(f'Cursor movido para o Nó {NodeValue}!')
+            
+        elif escolha=='13': #== == == Mostra o valor da raiz
+            print(arv.esvazia())
+        
+        elif escolha=='14':#== == == Mostra a quantidade de Nós
+            print('Quantidade de nós até agora: ', arv.__len__())
+        
+        elif escolha=='15':#== == == Encerra o programa
+            break
+        else:
+            raise(Exception('CHOICE NOT FOUND'))
+        input()
 
-preordem(raiz)
+    except NodeException as NE:
+        os.system('clear')
+        print(NE)
+        time.sleep(2)
+
+    except BinaryArborException as BAE:
+        os.system('clear')
+        print(BAE)
+        time.sleep(2)
+    except Exception as E:
+        os.system('clear')
+        print(E)
+        time.sleep(2)
+        
+'''
+arv = ArvoreBinaria(2)
+print('Criada a árvore')
+arv.addFilhoEsquerdo(7)
+arv.addFilhoDireito(5)
+print('Raiz: ', arv.getRaiz())
+print('Cursor: ', arv.getCursor())
+print('Tamanho: ', len(arv))
+arv.descerEsquerda()
+arv.addFilhoEsquerdo(9)
+arv.descerEsquerda()
+arv.addFilhoDireito(3)
+
+print('Raiz: ', arv.getRaiz())
+print('Cursor: ', arv.getCursor())
+print('Tamanho: ', len(arv))
+
+arv.resetCursor()
+arv.descerDireita()
+arv.addFilhoEsquerdo(10)
+
 print()
-emordem(raiz)
-print()
-posordem(raiz)
+arv.preordem()
 
+print('Raiz: ', arv.getRaiz())
+print('Cursor: ', arv.getCursor())
+print('Tamanho: ', len(arv))
 
-
+print('Busca:', arv.busca(3))
+no = arv.go(3)
+print('go:', no)'''
