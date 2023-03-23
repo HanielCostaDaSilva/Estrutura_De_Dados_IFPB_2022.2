e5e83fa3abc386443f6af4b2d76e5bf26dcb00b6
Haniel Costa da Silva <hanielcosta.ti@gmail.com>
2023-03-23 10:59:54 -0300

    adicionado alguns exercicios de pilha

--- Estrutura_De_Dados/Pilha/main.py
+++ Estrutura_De_Dados/Pilha/main.py
@@ -3,7 +3,57 @@ import time
 from pilhaEncadeada import *
 #from pilhaSequencial import *
 
+def concatenaPilhas( pilha1:Pilha, pilha2:Pilha )->Pilha:
+    '''Método que recebe duas pilhas e as concatena de maneira em que nenhuma delas perca qualquer dado. '''
+    pilhaAuxiliar1=Pilha() #pilha que irá receber todos os elementos da primeira pilha, sem desempilhar.  
+    pilhaAuxiliar2=Pilha() #pilha que irá receber todos os elementos da segunda pilha, sem desempilhar esta.
+    pilhaAuxiliar3=Pilha() #pilha que irá empilhar todos os elementos das pilhas auxiliar.
+    
+    for cursor in range(len(pilha1)):
+        '''Este laço é uma falsa representação de percorrimento de uma estrutura de dados. o cursor será responsável por indicar as posições das buscas pelos ELEMENTOS da pilha. '''
+        
+        NodeAtual= pilha1.elemento(cursor+1) #recebe o elemento cujo  valor está sendo apressentado no  cursor.
+        pilhaAuxiliar1.empilha(NodeAtual)
+        
+    for cursor in range(len(pilha2)):
+        NodeAtual= pilha2.elemento(cursor+1) #recebe o elemento cujo  valor está sendo apressentado no  cursor.
+        pilhaAuxiliar2.empilha(NodeAtual)
+    
+    
+        '''Nesta parte, a pilha que foi destinada receber a concatenação,"cls", irá empilhar os elementos das pilhas auxiliares.'''
+    while not(pilhaAuxiliar2.estaVazia()):
+        pilhaAuxiliar3.empilha(pilhaAuxiliar2.desempilha()) #Podemos utilizar o desempilha sem medo, pois foi gerado uma cópia dos dados, graças aos laços anteriores. 
+    
+    while not(pilhaAuxiliar1.estaVazia()):
+        pilhaAuxiliar3.empilha(pilhaAuxiliar1.desempilha()) #Podemos utilizar o desempilha sem medo, pois foi gerado uma cópia dos dados, graças aos laços anteriores.
+    
+    # print(pilha1)
+    # print(pilha2)
+    # print(pilhaAuxiliar1)
+    # print(pilhaAuxiliar2)
+    return pilhaAuxiliar3
+
+
+def checarPalindromo(frase:str):
+    pilhaAuxiliar=Pilha()
+    frase=frase.replace(',','')
+    frase=frase.replace(' ','')
+    frase=frase.replace('/','')
+    frase=frase.replace('*','')
+    frase=frase.replace('.','')
+    
+    for i in range(len(frase)):
+        pilhaAuxiliar.empilha(frase[i])
+    
+    for j in range(len(frase)):
+        elementoComparar=pilhaAuxiliar.desempilha()
+        
+        if frase[j] != elementoComparar: return False
+    
+    return True 
+
 def decToBin( numero:int):
+    '''Método que transforma um número decimal em binário '''
     pilhaAuxiliar=Pilha()
     while numero>=0:
         resto=numero%2
@@ -49,15 +99,15 @@ opcoesPilha={
     'L':'Limpar a Pilha',#8
     'C': 'Inverter Ordem da Pilha',#9
     'N':'Escolher Outra Pilha',#10
+    'Z':'Concatenar duas pilhas',
     'U':'Conversão dec/bin',
+    'O':'Checar Frase Palindroma',
     'F':'Finalizar o programa'#-
     
 }
-opcoesPilhachavesBusca= list(opcoesPilha.keys())
 
 pilhasLista=[None for i in range(10)]# O programa será multipilhas.
 posicaoPilhaAtual=0 #Posicao em que o usuario se encontra atualmente.
-
 
 #-- -- Preenchendo a Lista de Pilhas:
 
@@ -140,24 +190,43 @@ while True:
             
             print(decToBin(numeroDecimal))
             
+        elif escolha=='Z': #Concatenar duas pilhas diferentes
+            posicaoPilha1=int(input(f"[1 - {len(pilhasLista)}] 1ª Pilha\n Digite a pilha que deseja concatenar: "))-1
+            posicaoPilha2=int(input(f"[1 - {len(pilhasLista)}] 2ª Pilha\n Digite a segunda pilha que deseja concatenar: "))-1
+            destino=int(input(f"[1 - {len(pilhasLista)}] Pilha Destino\n Digite agora, qual a posição que será sobescrita: "))-1
+             
+            if (posicaoPilha1> len(pilhasLista) or posicaoPilha1<0) and \
+                (posicaoPilha2> len(pilhasLista) or posicaoPilha2<0) and \
+                (destino> len(pilhasLista) or destino<0): raise Exception('Posição inserida inválida')
+            
+            pilhasLista[destino]=concatenaPilhas(pilhasLista[posicaoPilha1],pilhasLista[posicaoPilha2])
+        
+        elif escolha=='O':
+            frase=input('Digite a frase que desejas checar se é palindroma: \n')
+
+            print(checarPalindromo(frase)) 
+            
         else:
             raise(Exception('CHOICE NOT FOUND'))
         input()
-        os.system('clear')
+        #os.system('clear')
 
 
-    except PilhaException as LE:
+    except PilhaException as PE:
         os.system('clear')
-        print(LE)
+        print(PE)
+        input()
         time.sleep(2)
     
     except TypeError as TE:
         os.system('clear')
         print(TE)
+        input()
         time.sleep(2)
     
     except Exception as E:
         os.system('clear')
         print(E)
+        input()
         time.sleep(2)
         
